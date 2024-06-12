cf6d695120e24b19a11ce63fde9c875e97be9300
AndinhoSKT <andinhoalderigi.1998@hotmail.com>
2024-06-12 20:22:21 -0300

    todos erros arrumados

---
+++ Entities/Department.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {           
+        }
+
+        public Department(string name){
+            Name = name;
+        }
+    }
+}
